c4d700efcd91d1798243e3148d5e9b9ccaff8092
Eduar Ramírez <eduar.ramirez.mt@correo.usa.edu.co>
2021-10-29 23:39:34 -0500

    first commit

---
+++ src/main/java/Retos_Ciclo3/Ciclo3/Ciclo3Application.java
@@ -0,0 +1,19 @@
+// 1. Modelo o entidad
+// 2. Interface
+// 3. Repositorio
+// 4. Servicios REST
+// 5. Controlador o WEB
+
+package Retos_Ciclo3.Ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ciclo3Application.class, args);
+	}
+
+}
